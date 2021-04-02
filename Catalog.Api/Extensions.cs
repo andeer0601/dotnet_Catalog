3da61d8196241623f22e3173f64bfdc4a4528faa
Anderson <48699535+andeer0601@users.noreply.github.com>
2021-04-02 12:28:33 -0300

    Tests imp

---
+++ Catalog.Api/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Api.Dtos;
+using Catalog.Api.Entities;
+
+namespace Catalog.Api
+{
+
+  public static class extensions
+{
+  public static ItemDto AsDto( this Item item )
+  {
+
+    return new ItemDto(item.Id, item.Name, item.Description, item.Price, item.CreateDate);
+
+  }
+}
+
+}
